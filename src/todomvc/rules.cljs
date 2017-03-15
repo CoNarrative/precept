21a803d19b1d800f1b160b9891a60257fbf43004
Alex Dixon <alex@conarrative.com>
2017-03-15 11:27:51 -0700

    Fact type fn works

--- src/todomvc/rules.cljs
+++ src/todomvc/rules.cljs
@@ -1,76 +1,103 @@
 (ns todomvc.rules
   (:require-macros [clara.macros :refer [defrule defquery defsession]])
-  (:require [clara.rules :refer [insert insert! insert-all! insert-all-unconditional!
+  (:require [clara.rules :refer [insert insert-all insert! insert-all! insert-all-unconditional!
                                  retract! query fire-rules]]
             [clara.rules.accumulators :as acc]))
 
 
 
-(defrecord Todo [id title done])
+;(defrecord Todo [id title done])
+;
+;(defrecord Showing [key])
+;
+;(defrecord VisibleTodos [todos])
+;
+;(defrecord ToggleComplete [])
+;
+;(defrule show-all
+;  [Showing (= key :all)]
+;  [?todos <- (acc/all) :from [Todo]]
+;  =>
+;  (prn "show-all rule fired" ?todos)
+;  (insert! (->VisibleTodos ?todos)))
+;
+;(defrule show-done
+;  [Showing (= key :done)]
+;  [?todos <- (acc/all) :from [Todo (= done true)]]
+;  =>
+;  (prn "show-done rule fired")
+;  (insert! (->VisibleTodos ?todos)))
+;
+;(defrule show-active
+;  [Showing (= key :active)]
+;  [?todos <- (acc/all) :from [Todo (= done false)]]
+;  =>
+;  (prn "show-active rule fired")
+;  (insert! (->VisibleTodos ?todos)))
+;
+;(defrule toggle-all-complete
+;  [?toggle <- ToggleComplete]
+;  [?todos <- (acc/all) :from [Todo]]
+;  =>
+;  (prn "toggle-all-complete rule fired")
+;  (prn "inserting" (map #(update % :done not) ?todos))
+;  (retract! ?toggle)
+;  (apply retract! ?todos)
+;  (insert-all-unconditional! (map #(update % :done not) ?todos)))
+;
+;(defquery find-showing
+;  []
+;  [?showing <- Showing])
+;
+;(defquery find-visible-todos
+;  []
+;  [?visible-todos <- VisibleTodos])
+;
+;(defquery find-todos
+;  []
+;  [?todos <- (acc/all) :from [Todo]])
+;
+;(defquery find-todo
+;  [:?id]
+;  [?todo <- Todo (= id ?id)])
+;
+;(defquery find-max-id
+;  []
+;  [?id <- (acc/max :id) :from [Todo]])
+;
+;(defquery find-all-done
+;  []
+;  [?todos <- (acc/all) :from [Todo (= done true)]])
+;
+;(defquery find-done-count
+;  []
+;  [?count <- (acc/count) :from [Todo (= done true)]])
+;
+(defquery really-find-todos
+  []
+  [?all <- (acc/all) :from [:todo/title]])
 
-(defrecord Showing [key])
-
-(defrecord VisibleTodos [todos])
-
-(defrecord ToggleComplete [])
-
-(defrule show-all
-  [Showing (= key :all)]
-  [?todos <- (acc/all) :from [Todo]]
+(defrule find-todos
+  [?x <- :todo/status [[e a v]] (= v :done)]
   =>
-  (prn "show-all rule fired" ?todos)
-  (insert! (->VisibleTodos ?todos)))
+  (println)
+  (println "Found------------------------------ " ?x))
 
-(defrule show-done
-  [Showing (= key :done)]
-  [?todos <- (acc/all) :from [Todo (= done true)]]
-  =>
-  (prn "show-done rule fired")
-  (insert! (->VisibleTodos ?todos)))
+(defn todo [v]
+  (let [temp-id (random-uuid)]
+    [[temp-id :todo/title v]
+     [temp-id :todo/status :done]]))
 
-(defrule show-active
-  [Showing (= key :active)]
-  [?todos <- (acc/all) :from [Todo (= done false)]]
-  =>
-  (prn "show-active rule fired")
-  (insert! (->VisibleTodos ?todos)))
+(todo "Hi")
 
-(defrule toggle-all-complete
-  [?toggle <- ToggleComplete]
-  [?todos <- (acc/all) :from [Todo]]
-  =>
-  (prn "toggle-all-complete rule fired")
-  (prn "inserting" (map #(update % :done not) ?todos))
-  (retract! ?toggle)
-  (apply retract! ?todos)
-  (insert-all-unconditional! (map #(update % :done not) ?todos)))
+(defsession todos 'todomvc.rules :fact-type-fn (fn [[e a v]]
+                                                 (println [e a v])
+                                                 a))
 
-(defquery find-showing
-  []
-  [?showing <- Showing])
+(def facts (reduce concat (repeatedly 5 #(todo "TODO"))))
 
-(defquery find-visible-todos
-  []
-  [?visible-todos <- VisibleTodos])
+(println "facts" facts)
 
-(defquery find-todos
-  []
-  [?todos <- (acc/all) :from [Todo]])
+(fire-rules (insert-all todos facts))
 
-(defquery find-todo
-  [:?id]
-  [?todo <- Todo (= id ?id)])
-
-(defquery find-max-id
-  []
-  [?id <- (acc/max :id) :from [Todo]])
-
-(defquery find-all-done
-  []
-  [?todos <- (acc/all) :from [Todo (= done true)]])
-
-(defquery find-done-count
-  []
-  [?count <- (acc/count) :from [Todo (= done true)]])
-
-(defsession todos 'todomvc.rules)
+(query session really-find-todos)
