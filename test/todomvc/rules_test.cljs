487ce809c27ecdd895b3195da51455ed9e4f54c5
Alex Dixon <alex@conarrative.com>
2017-03-17 14:10:57 -0700

    Add show all test

--- test/todomvc/rules_test.cljs
+++ test/todomvc/rules_test.cljs
@@ -6,27 +6,37 @@
                                    visibility-filter-tx
                                    toggle-tx
                                    map->tuple
+                                   entities-where
                                    find-done-count]]
             [clara.rules :refer [query insert insert-all fire-rules]]))
 
 (defn mk-todo [done]
   (todo-tx (random-uuid) "Title!" done))
 
-(defn mk-session-with [session facts]
+(defn insert-tuples [session tups]
+  (insert-all session (apply concat tups)))
+
+(defn insert-fire! [session facts]
   (-> session
-    (insert-all facts)
+    (insert-tuples facts)
     (fire-rules)))
 
 (deftest rules
   (testing "find-done-count"
     (testing "should return 0 if no todos done"
-      (let [facts   (apply concat (map map->tuple (repeatedly 5 #(mk-todo nil))))
-            session (mk-session-with todos facts)]
+      (let [facts   (map map->tuple (repeatedly 5 #(mk-todo nil)))
+            session (insert-fire! todos facts)]
         (println "facts" facts)
         (is (= 0 (:?count (first (query session find-done-count)))))))
     (testing "should return 5 if 5 todos done"
-      (let [facts  (apply concat (map map->tuple (repeatedly 5 #(mk-todo :done))))
-            session (mk-session-with todos facts)]
-        (is (= 5 (:?count (first (query session find-done-count)))))))))
+      (let [facts   (map map->tuple (repeatedly 5 #(mk-todo :done)))
+            session (insert-fire! todos facts)]
+        (is (= 5 (:?count (first (query session find-done-count))))))))
 
+  (testing "show-all"
+    (testing "datoms with :todo/visible should equal num of todos
+              Note: :todo/visible is false by default at insertion time"
+      (let [facts   (map map->tuple (repeatedly 5 #(mk-todo :done)))
+            session (insert-fire! todos facts)]
+        (is (= 0 (count (entities-where session :todo/visible true))))))))
 (run-tests)
