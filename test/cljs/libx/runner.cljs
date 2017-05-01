835e2e48479c27fa1c234b8aff0af64d0e8284f7
Alex Dixon <alex@conarrative.com>
2017-05-01 15:43:07 -0700

    Work on getting CLJS test environment to work

--- test/cljs/libx/runner.cljs
+++ test/cljs/libx/runner.cljs
@@ -1,10 +1,10 @@
 (ns libx.runner
    (:require [libx.subscribe-test]
-             [libx.store-test]))
-             ;[doo.runner :refer-macros [doo-tests]]))
+             [libx.store-test]
+             [doo.runner :refer-macros [doo-tests]]))
 
-;(doo-tests 'libx.subscribe-test
-;           'libx.store-test))
+(doo-tests 'libx.subscribe-test
+           'libx.store-test)
 
 (defn ^:export main []
   (enable-console-print!)
