2d73f2c843d6c12f15ff7cd02282dd02cc2cfb39
Alex Dixon <alex@conarrative.com>
2017-05-04 08:34:32 -0700

    Add planck bash script

--- bin/planck.sh
+++ bin/planck.sh
@@ -1,1 +1,13 @@
 #!/usr/bin/env bash
+
+# Starts `planck` self-hosted CLJS repl on localhost:7777 with lein classpath loaded
+#
+# To use with Cursive/IntelliJ REPL:
+# 1. Create configuration for local / clojure-main
+# 2. Enter the following at the REPL prompt (assumes tubular is a dependency)
+#   (require 'tubular.core)
+#   (tubular.core/connect 7777)
+# 3. In REPL window use dropdown to change clj repl to cljs repl
+
+
+planck -c`lein classpath` -n 7777
