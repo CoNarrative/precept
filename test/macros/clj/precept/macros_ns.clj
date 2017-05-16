6bcf123de3d8a51ba74750faea8b09b500b5cd14
Alex Dixon <alex@conarrative.com>
2017-05-16 07:52:24 -0700

    Macro experiments after SO question

--- test/macros/clj/precept/macros_ns.clj
+++ test/macros/clj/precept/macros_ns.clj
@@ -1,13 +1,16 @@
-(ns precept.macros-ns)
+(ns precept.macros-ns
+  (:require [clara.rules.compiler :as com]))
 
 (defmacro inner-macro [s]
   `['(my-ns/my-fn) :from ['~s :all]])
 
 (defmacro outer-macro [y xs]
+  (let [_ (println "CLJS NS outer macro" (com/cljs-ns))])
   `(into ['~y '~'<-] (eval '~xs)))
 
 (defmacro macro-context [other-macros]
   (let [symbols (eval other-macros)
+        _ (println "CLJS NS" (com/cljs-ns))
         _ (println "Expanded in macro context" symbols)]
     {:result `(list '~symbols '~'+ '~'further-rearrangement)}))
 
